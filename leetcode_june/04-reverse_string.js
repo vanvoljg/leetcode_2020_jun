8f012400b7fe817e1e9e300779e0bfd675e4134a
Jesse Van Volkinburg <vanvoljg@gmail.com>
2020-06-04 15:41:57 -0700

    day 4
    reverse an array of characters

---
+++ leetcode_june/04-reverse_string.js
@@ -0,0 +1,13 @@
+let reverseString = function (s) {
+  let left = 0;
+  let right = s.length - 1;
+  while (left < right) {
+    [s[left], s[right]] = [s[right], s[left]];
+    left++;
+    right--;
+  }
+};
+
+let s = [...'hello'];
+reverseString(s);
+console.log(s);
