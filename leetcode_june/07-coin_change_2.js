c472b1fcb4833bfb15166a55ae59747d46b182a5
Jesse Van Volkinburg <vanvoljg@gmail.com>
2020-06-07 13:36:41 -0700

    method takes too long
    Need to research better ways of doing this. Memoization or similar, because this algo checks a lot of extraneous options that can't work, and multiple times as well...

--- leetcode_june/07-coin_change_2.js
+++ leetcode_june/07-coin_change_2.js
@@ -3,11 +3,30 @@
  * @param {number[]} coins
  * @return {number}
  */
-const change = function(amount, coins) {
+const change = function (amount, coins) {};
 
+const count = function (coins, coinIdx, amount) {
+  if (amount == 0) return 1;
+  if (amount < 0) return 0;
+  if (coinIdx <= 0 && amount > 0) return 0;
+  return (
+    count(coins, coinIdx - 1, amount) +
+    count(coins, coinIdx, amount - coins[coinIdx - 1])
+  );
 };
 
 /*
  * 500
  * [3,5,7,8,9,10,11]
-*/
+ */
+
+const run = function () {
+  const amount = 500;
+  const coins = [3, 5, 7, 8, 9, 10, 11];
+  const start = Date.now();
+  const number = count(coins, coins.length, amount);
+  const duration = Date.now() - start;
+  console.log({ number, duration });
+};
+
+run();
