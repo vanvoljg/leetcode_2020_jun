882c4942810445d5ef0aa77d9779b197effce335
Jesse Van Volkinburg <vanvoljg@gmail.com>
2020-06-09 12:42:34 -0700

    algo complete

--- leetcode_june/09-is_subsequence.js
+++ leetcode_june/09-is_subsequence.js
@@ -4,5 +4,18 @@
  * @return {boolean}
  */
 var isSubsequence = function (s, t) {
+  const tEnd = t.length;
+  const sEnd = s.length;
+  let j = 0;
 
+  // This loop will not check end condition if end of t was reached
+  for (let i = 0; i < tEnd; i++) {
+    if (j >= sEnd) return true;
+    if (t[i] == s[j]) {
+      j++;
+    }
+  }
+  
+  // Performs the missing end condition check from above loop
+  return j >= sEnd;
 };
