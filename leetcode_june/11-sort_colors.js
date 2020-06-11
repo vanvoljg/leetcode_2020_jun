de8ca7bdaeffafda85f35ba7f9998b7010e8fbf2
Jesse Van Volkinburg <vanvoljg@gmail.com>
2020-06-11 12:49:00 -0700

    optimizations

--- leetcode_june/11-sort_colors.js
+++ leetcode_june/11-sort_colors.js
@@ -3,23 +3,26 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 const sortColors = (nums) => {
+  if (nums.length < 2) return;
   let idxR = 0;
-  let idxW = 0;
-  let idxB = nums.length;
+  let idxB = nums.length - 1;
   let i = 0;
 
-  while (i < idxB) {
-    if (nums[i] == 0) {
-      if (i != idxR) arraySwap(nums, i, idxR);
-      idxR++;
-      i++;
-    } else if (nums[i] == 2) {
-      arraySwap(nums, i, idxB - 1);
-      idxB--;
-    } else if (nums[i] ==1 ) {
-      i++;
-    } else {
-      console.log('Found invalid array element');
+  while (i <= idxB) {
+    switch (nums[i]) {
+      case 0:
+        i != idxR && arraySwap(nums, i, idxR);
+        idxR++;
+      case 1:
+        i++;
+        break;
+      case 2:
+        arraySwap(nums, i, idxB);
+        idxB--;
+        break;
+      default:
+        console.log('Found invalid array element');
+        return;
     }
   }
   return;
@@ -30,14 +33,23 @@ const arraySwap = (arr, l, r) => {
   arr[l] = arr[r];
   arr[r] = tmp;
   return;
-}
+};
 
 const run = () => {
-  let arr = [2,0,2,1,1,0];
+  // let arr = randArr(100);
+  let arr = [ 1, 0, 1, 2, 0, 0, 1, 0, 2, 0, 0, 1, 1, 0, 0, 2, 2, 1, 0, 2, 1, 2, 1, 1, 1, 2, 1, 2, 1, 2, 1, 2, 2, 1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 2, 0, 1, 0, 1, 2, 2, 2, 0, 0, 2, 2, 2, 2, 1, 0, 1, 0, 1, 2, 2, 0, 2, 1, 0, 1, 0, 1, 2, 1, 1, 0, 0, 2, 2, 0, 0, 1, 2, 2, 1, 0, 1, 0, 0, 2, 2, 2, 0, 0, 0, 2, 1, 1, 1, ];
 
-  console.log("Before:", {arr});
+  console.log('Before:', { arr });
   sortColors(arr);
-  console.log("After:", {arr});
+  console.log('After:', { arr });
+};
+
+const randArr = (n) => {
+  let ret = new Array(n);
+  for (let i = 0; i < ret.length; i++) {
+    ret[i] = Math.floor(Math.random() * 3);
+  }
+  return ret;
 };
 
 run();
