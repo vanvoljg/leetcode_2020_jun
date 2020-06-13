bcd26568a6c05d652dea3957208e267b7d096a13
Jesse Van Volkinburg <vanvoljg@gmail.com>
2020-06-13 13:56:06 -0700

    algo complete...?
    This is an O(n^2) solution, which I'm not happy with...
    
    Oh, it doesn't handle a couple cases... never mind. Back to research.

--- leetcode_june/13-largest_divisible_subset.js
+++ leetcode_june/13-largest_divisible_subset.js
@@ -3,18 +3,40 @@
  * @return {number[]}
  */
 const largestDivisibleSubset = (nums) => {
+  if (nums.length <= 1) return nums;
 
+  let ret = [nums[0]];
+  for (let i = 1; i < nums.length; i++) {
+    const retLen = ret.length;
+    for (j = 0; j < retLen; j++) {
+      if (nums[i] % ret[j] != 0 && ret[j] % nums[i] != 0) {
+        ret.splice(j, 1);
+      } else {
+        if (ret[ret.length - 1] != nums[i]) {
+          ret.push(nums[i]);
+        }
+      }
+    }
+  }
+
+  return ret;
 };
 
 const run = () => {
-  let in0 = [1,2,3];
-  let in1 = [1,2,4,8];
+  let in0 = [1, 2, 3];
+  let in1 = [1, 2, 4, 8];
+  let in2 = [];
+  let in3 = [5, 2, 10, 15, 4, 8, 32, 44];
 
   console.log({
     in0,
     out0: largestDivisibleSubset(in0),
     in1,
     out1: largestDivisibleSubset(in1),
+    in2,
+    out2: largestDivisibleSubset(in2),
+    in3,
+    out3: largestDivisibleSubset(in3),
   });
 };
 
