c9ec2961b020be4a5a688690a271a28efba8fd6f
Jesse Van Volkinburg <vanvoljg@gmail.com>
2020-06-17 11:57:23 -0700

    added test cases and runner

--- leetcode_june/17-surrounded_regions.js
+++ leetcode_june/17-surrounded_regions.js
@@ -2,5 +2,54 @@
  * @param {character[][]} board
  * @return {void} Do not return anything, modify board in-place instead.
  */
-const solve = (board) => {
+const solve = (board) => {};
+
+const testRunner = (testArr) => {
+  for (const [inBoard, outBoard] of testArr) {
+    console.log({
+      inBoard,
+      outBoard,
+      pass: solve(inBoard) === outBoard,
+    });
+  }
 };
+
+const run = () => {
+  let boards = [
+    [
+      [
+        ['X', 'X', 'X', 'X'],
+        ['X', 'O', 'O', 'X'],
+        ['X', 'X', 'O', 'X'],
+        ['X', 'O', 'X', 'X'],
+      ],
+      [
+        ['X', 'X', 'X', 'X'],
+        ['X', 'X', 'X', 'X'],
+        ['X', 'X', 'X', 'X'],
+        ['X', 'O', 'X', 'X'],
+      ],
+    ],
+    [
+      [
+        ['X', 'X', 'X', 'X', 'X'],
+        ['X', 'O', 'O', 'X', 'X'],
+        ['X', 'X', 'O', 'X', 'X'],
+        ['X', 'O', 'X', 'X', 'X'],
+        ['X', 'X', 'O', 'X', 'O'],
+      ],
+      [
+        ['X', 'X', 'X', 'X', 'X'],
+        ['X', 'X', 'X', 'X', 'X'],
+        ['X', 'X', 'X', 'X', 'X'],
+        ['X', 'X', 'X', 'X', 'X'],
+        ['X', 'X', 'O', 'X', 'O'],
+      ],
+    ],
+  ];
+  console.log(boards);
+
+  testRunner(boards);
+};
+
+run();
