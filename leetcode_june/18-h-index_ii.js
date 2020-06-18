8db10c9ea345fb785f89e8844149316f3201f754
Jesse Van Volkinburg <vanvoljg@gmail.com>
2020-06-18 11:59:53 -0700

    failure, failure, failure
    Okay, I've got to look up a valid answer. My brain is apparently completely useless and I feel really stupid.

--- leetcode_june/18-h-index_ii.js
+++ leetcode_june/18-h-index_ii.js
@@ -4,6 +4,15 @@
  */
 const hIndex = (citations) => {
   if (citations.length === 0) return 0;
+  if (citations.length === 1 && citations[0] !== 0) return 1;
+  if (citations.length < 5) {
+    for (let i = citations.length - 1; i >= 0; i--) {
+      const paperNum = citations.length - i;
+      if (citations[i] < paperNum) {
+        return paperNum - 1;
+      }
+    }
+  }
   let l = 0;
   let r = citations.length - 1;
   let mid = 0;
@@ -28,7 +37,7 @@ const testRunner = (tests) => {
     console.log({
       citations,
       hIdx,
-      result, 
+      result,
       pass: result === hIdx,
     });
   }
@@ -39,12 +48,42 @@ const test = () => {
     [[0, 1, 3, 5, 6], 3],
     [[3, 3, 5, 8, 25], 3],
     [[3, 4, 5, 8, 10], 4],
-    [[0,0,3,4,7,9,10,11,11,18,20,22],7],
-    [[0,0,3,4,7,7,7,8,8,9,9,9,10,11,11,18,20,22],9],
-    [[0,0,3,4,7,7,7,8,8,8,8,8,8,8,8,9,9,9,10,11,11,18,20,22],9],
-    [[],0],
-    [[1],1],
-    [[0],0],
+    [[0, 0, 3, 4, 7, 9, 10, 11, 11, 18, 20, 22], 7],
+    [[0, 0, 3, 4, 7, 7, 7, 8, 8, 9, 9, 9, 10, 11, 11, 18, 20, 22], 9],
+    [
+      [
+        0,
+        0,
+        3,
+        4,
+        7,
+        7,
+        7,
+        8,
+        8,
+        8,
+        8,
+        8,
+        8,
+        8,
+        8,
+        9,
+        9,
+        9,
+        10,
+        11,
+        11,
+        18,
+        20,
+        22,
+      ],
+      9,
+    ],
+    [[], 0],
+    [[1], 1],
+    [[0], 0],
+    [[100], 1],
+    [[1, 2], 1],
   ];
 
   testRunner(citations);
