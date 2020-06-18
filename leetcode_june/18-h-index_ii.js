9393e2d2f5ea3f6e4dd82ae652d02a41fa0c64a9
Jesse Van Volkinburg <vanvoljg@gmail.com>
2020-06-18 11:46:22 -0700

    algo improvement
    Binary search works. I'm still a tad fuzzy on what I'm doing here, but the formula is working correctly, and it's the canonical formula based on Wikipedia. The sorting of the input array is reversed, so paperNum must be counted from the array length - current index.

--- leetcode_june/18-h-index_ii.js
+++ leetcode_june/18-h-index_ii.js
@@ -4,20 +4,32 @@
  */
 const hIndex = (citations) => {
   // Because it's easier to search from the largest number, go in reverse order
-  for (let i = citations.length - 1; i >= 0; i--) {
-    const paperNum = citations.length - i;
-    if (citations[i] < paperNum) {
-      return paperNum - 1;
+  let l = 0;
+  let r = citations.length - 1;
+  let mid = 0;
+  let paperNum = 0;
+  while (l <= r) {
+    mid = ((r - l) >> 1) + l;
+    paperNum = citations.length - mid;
+    if (citations[mid] < paperNum) {
+      l = mid + 1;
+    } else if (citations[mid] > paperNum) {
+      r = mid - 1;
+    } else if (citations[mid] === paperNum) {
+      return paperNum;
     }
   }
+  return paperNum - 1;
 };
 
 const testRunner = (tests) => {
   for (const [citations, hIdx] of tests) {
+    const result = hIndex(citations);
     console.log({
       citations,
       hIdx,
-      pass: hIndex(citations) === hIdx,
+      result, 
+      pass: result === hIdx,
     });
   }
 };
@@ -27,6 +39,9 @@ const test = () => {
     [[0, 1, 3, 5, 6], 3],
     [[3, 3, 5, 8, 25], 3],
     [[3, 4, 5, 8, 10], 4],
+    [[0,0,3,4,7,9,10,11,11,18,20,22],7],
+    [[0,0,3,4,7,7,7,8,8,9,9,9,10,11,11,18,20,22],9],
+    [[0,0,3,4,7,7,7,8,8,8,8,8,8,8,8,9,9,9,10,11,11,18,20,22],9],
   ];
 
   testRunner(citations);
