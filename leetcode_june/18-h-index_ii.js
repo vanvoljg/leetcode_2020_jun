3d82e069f40e71b5b74b4b21eb1ee4c89c7e156c
Jesse Van Volkinburg <vanvoljg@gmail.com>
2020-06-18 13:24:12 -0700

    solution accepted...
    Totally stolen from this guy: https://leetcode.com/problems/h-index-ii/discuss/71063/Standard-binary-search
    
    I don't understand why my multiple failures are particularly different from this, but that's okay, I don't expect to understand anything, anymore. Stolen solutions forevermore.

--- leetcode_june/18-h-index_ii.js
+++ leetcode_june/18-h-index_ii.js
@@ -6,19 +6,19 @@
 const hIndex = (citations) => {
   let l = 0;
   let r = citations.length - 1;
-  let i = 0;
-  let h = 0;
+  let mid = 0;
   while (l <= r) {
-    i = ((r - l) >> 1) + l;
-    const paperIdx = citations.length - i; // 1-based indexing
-    if (citations[i] >= paperIdx) {
-      r = i - 1;
-    } else if (citations[i] < paperIdx) {
-      l = i + 1;
-    } 
-    h = paperIdx;
+    mid = ((r - l) >> 1) + l;
+    const paperIdx = citations.length - mid;
+    if (citations[mid] === paperIdx) {
+      return citations[mid];
+    } else if (citations[mid] > paperIdx) {
+      r = mid - 1;
+    } else { // citations[mid] < paperIdx
+      l = mid + 1;
+    }
   }
-  return citations[i] >= citations[i - 1] ? h - 1 : h;
+  return citations.length - r - 1;
 };
 
 const testRunner = (tests) => {
@@ -40,12 +40,31 @@ const test = () => {
     [[3, 4, 5, 8, 10], 4],
     [[0, 0, 3, 4, 7, 9, 10, 11, 11, 18, 20, 22], 7],
     [[0, 0, 3, 4, 7, 7, 7, 8, 8, 9, 9, 9, 10, 11, 11, 18, 20, 22], 9],
+    [[0, 3, 4, 7, 9, 10, 11, 18, 20, 22, 24, 25, 28, 30, 33, 50, 102], 11],
     [
-      [0, 3, 4, 7, 9, 10, 11, 18, 20, 22, 24, 25, 28, 30, 33, 50, 102],
-      11,
-    ],
-    [
-      [0, 3, 4, 7, 9, 10, 11, 18, 20, 22, 24, 25, 28, 30, 33, 35, 42, 50, 62, 71, 102],//len 21
+      [
+        0,
+        3,
+        4,
+        7,
+        9,
+        10,
+        11,
+        18,
+        20,
+        22,
+        24,
+        25,
+        28,
+        30,
+        33,
+        35,
+        42,
+        50,
+        62,
+        71,
+        102,
+      ], //len 21
       14,
     ],
     [[], 0],
