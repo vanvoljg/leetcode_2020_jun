a6c592277a6f863bffa0089b6ba60018625b46d3
Jesse Van Volkinburg <vanvoljg@gmail.com>
2020-06-18 11:23:22 -0700

    algo complete
    Initial pass based on math formula on Wikipedia. Need to convert the for loop to a binary search to reduce the time on large sets.

--- leetcode_june/18-h-index_ii.js
+++ leetcode_june/18-h-index_ii.js
@@ -2,7 +2,15 @@
  * @param {number[]} citations
  * @return {number}
  */
-const hIndex = (citations) => {};
+const hIndex = (citations) => {
+  // Because it's easier to search from the largest number, go in reverse order
+  for (let i = citations.length - 1; i >= 0; i--) {
+    const paperNum = citations.length - i;
+    if (citations[i] < paperNum) {
+      return paperNum - 1;
+    }
+  }
+};
 
 const testRunner = (tests) => {
   for (const [citations, hIdx] of tests) {
