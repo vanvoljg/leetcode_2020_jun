3a319f33df9d401253fc0b7886b1d5fdadbfa9b3
Jesse Van Volkinburg <vanvoljg@gmail.com>
2020-06-18 11:12:33 -0700

    added tests and test runner

--- leetcode_june/18-h-index_ii.js
+++ leetcode_june/18-h-index_ii.js
@@ -2,6 +2,26 @@
  * @param {number[]} citations
  * @return {number}
  */
-const hIndex = (citations) => {
-    
+const hIndex = (citations) => {};
+
+const testRunner = (tests) => {
+  for (const [citations, hIdx] of tests) {
+    console.log({
+      citations,
+      hIdx,
+      pass: hIndex(citations) === hIdx,
+    });
+  }
 };
+
+const test = () => {
+  let citations = [
+    [[0, 1, 3, 5, 6], 3],
+    [[3, 3, 5, 8, 25], 3],
+    [[3, 4, 5, 8, 10], 4],
+  ];
+
+  testRunner(citations);
+};
+
+test();
