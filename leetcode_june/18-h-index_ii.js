ff2bafbf7d8d196cf94797a8258e590569ae2d53
Jesse Van Volkinburg <vanvoljg@gmail.com>
2020-06-18 13:06:45 -0700

    more failure
    I tried again. I failed. I suck.

--- leetcode_june/18-h-index_ii.js
+++ leetcode_june/18-h-index_ii.js
@@ -1,34 +1,24 @@
+'use strict';
 /**
  * @param {number[]} citations
  * @return {number}
  */
 const hIndex = (citations) => {
-  if (citations.length === 0) return 0;
-  if (citations.length === 1 && citations[0] !== 0) return 1;
-  if (citations.length < 5) {
-    for (let i = citations.length - 1; i >= 0; i--) {
-      const paperNum = citations.length - i;
-      if (citations[i] < paperNum) {
-        return paperNum - 1;
-      }
-    }
-  }
   let l = 0;
   let r = citations.length - 1;
-  let mid = 0;
-  let paperNum = 0;
+  let i = 0;
+  let h = 0;
   while (l <= r) {
-    mid = ((r - l) >> 1) + l;
-    paperNum = citations.length - mid;
-    if (citations[mid] < paperNum) {
-      l = mid + 1;
-    } else if (citations[mid] > paperNum) {
-      r = mid - 1;
-    } else if (citations[mid] === paperNum) {
-      return paperNum;
-    }
+    i = ((r - l) >> 1) + l;
+    const paperIdx = citations.length - i; // 1-based indexing
+    if (citations[i] >= paperIdx) {
+      r = i - 1;
+    } else if (citations[i] < paperIdx) {
+      l = i + 1;
+    } 
+    h = paperIdx;
   }
-  return paperNum - 1;
+  return citations[i] >= citations[i - 1] ? h - 1 : h;
 };
 
 const testRunner = (tests) => {
@@ -51,39 +41,19 @@ const test = () => {
     [[0, 0, 3, 4, 7, 9, 10, 11, 11, 18, 20, 22], 7],
     [[0, 0, 3, 4, 7, 7, 7, 8, 8, 9, 9, 9, 10, 11, 11, 18, 20, 22], 9],
     [
-      [
-        0,
-        0,
-        3,
-        4,
-        7,
-        7,
-        7,
-        8,
-        8,
-        8,
-        8,
-        8,
-        8,
-        8,
-        8,
-        9,
-        9,
-        9,
-        10,
-        11,
-        11,
-        18,
-        20,
-        22,
-      ],
-      9,
+      [0, 3, 4, 7, 9, 10, 11, 18, 20, 22, 24, 25, 28, 30, 33, 50, 102],
+      11,
+    ],
+    [
+      [0, 3, 4, 7, 9, 10, 11, 18, 20, 22, 24, 25, 28, 30, 33, 35, 42, 50, 62, 71, 102],//len 21
+      14,
     ],
     [[], 0],
     [[1], 1],
     [[0], 0],
     [[100], 1],
     [[1, 2], 1],
+    [[11, 15], 2],
   ];
 
   testRunner(citations);
