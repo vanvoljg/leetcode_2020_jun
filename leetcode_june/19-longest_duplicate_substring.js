161d71779485b513d6c9e311318008bff45e822d
Jesse Van Volkinburg <vanvoljg@gmail.com>
2020-06-19 23:08:49 -0700

    notes and a hash function

--- leetcode_june/19-longest_duplicate_substring.js
+++ leetcode_june/19-longest_duplicate_substring.js
@@ -4,8 +4,21 @@
  * @return {string}
  */
 const longestDupSubstring = (S) => {
+  // Hint suggests using binary searching to find the correct length of the
+  // longest substring.
+  // Research suggested taking that length and trying all possible substrings
+  // of that length
+  if (S.length <= 1) return ""; // No way to have a duplicate with 0 or 1 chars
 
 };
+
+const hash = (string, q, r = 26) => {
+  let h = 0;
+  for (let i = 0; i < string.length; i++) {
+    h = (r * h + string.charCodeAt(i)) % q;
+  }
+  return h;
+}
 
 const testRunner = (tests) => {
   for (const [string, answer] of tests) {
