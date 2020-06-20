5970ae873a5097471bb49c0a06983717f5a83fe4
Jesse Van Volkinburg <vanvoljg@gmail.com>
2020-06-19 17:08:41 -0700

    added test cases and runner

--- leetcode_june/19-longest_duplicate_substring.js
+++ leetcode_june/19-longest_duplicate_substring.js
@@ -1,7 +1,31 @@
+'use strict';
 /**
  * @param {string} S
  * @return {string}
  */
 const longestDupSubstring = (S) => {
-  
+
 };
+
+const testRunner = (tests) => {
+  for (const [string, answer] of tests) {
+    const result = longestDupSubstring(string);
+    console.log({
+      string,
+      answer,
+      result,
+      pass: result === answer,
+    });
+  }
+};
+
+const test = () => {
+  const tests = [
+    ['banana', 'ana'],
+    ['abcde', ''],
+  ];
+
+  testRunner(tests);
+};
+
+test();
