8814578933dfc9d111634843bfbec769fd5ade58
Jesse Van Volkinburg <vanvoljg@gmail.com>
2020-06-20 21:57:02 -0700

    still not working

--- leetcode_june/20-permutation_sequence.js
+++ leetcode_june/20-permutation_sequence.js
@@ -16,14 +16,24 @@ const permutations = (n) => {
   let j = 0;
   let limit = arr.length * n;
   const nStr = String(n);
-  console.log({ limit, 'arr.length': arr.length });
   while (j < limit) {
-    const cur = arr[i].split('');
-    cur.splice(n - (j % n) - 1, 0, nStr);
-    ret[j] = cur.join('');
+    console.log({ i, j , n});
+    let cur;
+    if (i < n - 1) {
+      cur = arr[i].split('');
+      cur.splice(n - (j % (n-1)) - 1, 0, nStr);
+      ret[j] = cur.join('');
+    } else {
+      console.log('blib');
+      for (let el of arr) {
+        cur = el.split('');
+        cur.splice(0, 0, nStr);
+        ret[j] = cur.join('');
+        j++;
+      }
+    }
     j++;
-    if (j % n === 0) i++;
-    // console.log({ i, j });
+    if (j % (n - 1) === 0) i++;
   }
   console.log({ arr, ret });
 
@@ -47,10 +57,10 @@ const testRunner = (tests, func) => {
 const test = () => {
   const tests = [
     // [[1, 1], '1'],
-    [[2, 1], '12'],
+    // [[2, 1], '12'],
     // [[2, 2], '21'],
-    [[3, 3], '213'],
-    // [[4,9],'2314'],
+    // [[3, 3], '213'],
+    [[4,9],'2314'],
     // [[4,17],'3412'],
   ];
 
