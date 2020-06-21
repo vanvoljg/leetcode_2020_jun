bebee46d91700a7cf407386e0c34766a51dbc87d
Jesse Van Volkinburg <vanvoljg@gmail.com>
2020-06-21 16:45:55 -0700

    added test and runner

--- leetcode_june/21-dungeon_game.js
+++ leetcode_june/21-dungeon_game.js
@@ -5,3 +5,27 @@
 const calculateMinimumHP = function(dungeon) {
   
 };
+
+const testRunner = (tests, func) => {
+  const name = func.name;
+  for (const [input, expected] of tests) {
+    const result = func(input);
+    console.log({
+      name,
+      input,
+      expected,
+      result,
+      pass: result === expected,
+    });
+  }
+};
+
+const test = () => {
+  const tests = [
+    [[[-2,-3,3],[-5,-10,1],[10,30,-5]], 7],
+  ];
+
+  testRunner(tests, calculateMinimumHP);
+};
+
+test();
