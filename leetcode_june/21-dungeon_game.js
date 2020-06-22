5d54d182b92edd941d55c2f232f9404b59ce6a99
Jesse Van Volkinburg <vanvoljg@gmail.com>
2020-06-21 19:01:25 -0700

    solution complete
    Solution based heavily on https://leetcode.com/problems/dungeon-game/discuss/698935/Python-3-DP-Solution-Easy-to-understand-explanation!-O\(M\*N\)

--- leetcode_june/21-dungeon_game.js
+++ leetcode_june/21-dungeon_game.js
@@ -2,8 +2,38 @@
  * @param {number[][]} dungeon
  * @return {number}
  */
-const calculateMinimumHP = function(dungeon) {
-  
+const calculateMinimumHP = function (dungeon) {
+  const h = dungeon.length;
+  const w = dungeon[0].length;
+  if (h == 0 || w == 0) return 0;
+
+  let hp = new Array(h).fill(0);
+  for (let i = 0; i < h; i++) {
+    hp[i] = new Array(w).fill(0);
+  }
+
+  hp[h - 1][w - 1] = Math.max(1, 1 - dungeon[h - 1][w - 1]);
+
+  // build right column
+  const right = w - 1;
+  for (let i = h - 2; i >= 0; i--) {
+    hp[i][right] = Math.max(1, hp[i + 1][right] - dungeon[i][right]);
+  }
+
+  // build bottom row
+  const bottom = h - 1;
+  for (let j = w - 2; j >= 0; j--) {
+    hp[bottom][j] = Math.max(1, hp[bottom][j + 1] - dungeon[bottom][j]);
+  }
+
+  // remaining cells
+  for (let i = h - 2; i >= 0; i--) {
+    for (let j = w - 2; j >= 0; j--) {
+      hp[i][j] = Math.max(1, Math.min(hp[i + 1][j], hp[i][j + 1]) - dungeon[i][j])
+    }
+  }
+
+  return hp[0][0];
 };
 
 const testRunner = (tests, func) => {
@@ -22,7 +52,27 @@ const testRunner = (tests, func) => {
 
 const test = () => {
   const tests = [
-    [[[-2,-3,3],[-5,-10,1],[10,30,-5]], 7],
+    [
+      [
+        [-2, -3, 3],
+        [-5, -10, 1],
+        [10, 30, -5],
+      ],
+      7,
+    ],
+    [
+      [
+        [  0, -10, -10, -10, -10, -10],
+        [  0,   0,   0,   0,   0, -10],
+        [-10, -10, -10, -10,   0, -10],
+        [  0,   0,   0, -10,   0, -10],
+        [  0, -10,   0, -10,   0, -10],
+        [  0, -10,   0,   0,   0, -10],
+        [  0, -10, -10, -10, -10, -10],
+        [  0,   0,   0,   0,   0,   0],
+      ],
+      11,
+    ],
   ];
 
   testRunner(tests, calculateMinimumHP);
