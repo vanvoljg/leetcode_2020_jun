a3682386b7cd7a2427ddbd3ac4506315a432b4a2
Jesse Van Volkinburg <vanvoljg@gmail.com>
2020-06-22 15:56:36 -0700

    add tests and runner

--- leetcode_june/22-single_number_ii.js
+++ leetcode_june/22-single_number_ii.js
@@ -5,3 +5,28 @@
 const singleNumber = function(nums) {
   
 };
+
+const testRunner = (tests, func) => {
+  const name = func.name;
+  for (const [input, expected] of tests) {
+    const result = func(input);
+    console.log({
+      name,
+      input,
+      expected,
+      result,
+      pass: result === expected,
+    });
+  }
+};
+
+const test = () => {
+  const tests = [
+    [[2,2,3,2],3],
+    [[0,1,0,1,0,1,99],99],
+  ];
+
+  testRunner(tests, singleNumber);
+};
+
+test();
