3b13dc0a3c35f1c309b51d58125eae9b4b163f79
Jesse Van Volkinburg <vanvoljg@gmail.com>
2020-06-23 12:45:35 -0700

    add tests and runner

--- leetcode_june/23-count_complete_tree_nodes.js
+++ leetcode_june/23-count_complete_tree_nodes.js
@@ -1,15 +1,38 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
+'use strict';
+
+import { Tree } from './lib/tree.js';
+
 /**
  * @param {TreeNode} root
  * @return {number}
  */
-const countNodes = function(root) {
-  
+const countNodes = function (root) {
+  return;
 };
+
+const testRunner = (tests, func) => {
+  const name = func.name;
+  for (const [treeArray, expected] of tests) {
+    let input = new Tree(treeArray);
+    const result = func(input);
+    console.log({
+      name,
+      input: input.toArray(),
+      expected,
+      result,
+      pass: result === expected,
+    });
+  }
+};
+
+const test = () => {
+  const tests = [
+    [[1, 2, 3, 4, 5, 6], 6],
+    [[1, 2, 3, 4], 4],
+    [[1, 2, 3, 4, 5, 6, 7], 7],
+  ];
+
+  testRunner(tests, countNodes);
+};
+
+export default test();
