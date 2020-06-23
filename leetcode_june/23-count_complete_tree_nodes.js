8c90265b440a5b4261174a291f534e8752425c40
Jesse Van Volkinburg <vanvoljg@gmail.com>
2020-06-23 13:19:24 -0700

    algo complete
    Solution accepted. There is a fancier way of doing this, which has a range of times it can hit, between O(n) and O(lg n), but sometimes the simple way is easier. The fancier way of doing it involves going left until you can't anymore, and taking that depth (minus 1) and taking 2^(depth-1) to get the number of nodes above the bottom level, then you need to stay on the bottom level and continually traverse from far right to far left, which basically ends up being a BFS kind of search... so actually, I just convinced myself that the simple solution of just counting is actually probably just as good as any other. Simpler to debug, too.

--- leetcode_june/23-count_complete_tree_nodes.js
+++ leetcode_june/23-count_complete_tree_nodes.js
@@ -7,14 +7,23 @@ import { Tree } from './lib/tree.js';
  * @return {number}
  */
 const countNodes = function (root) {
-  return;
+  // Brute-force O(n) solution: DFS counting
+  let count = dfsCount(root);
+  return count;
 };
+
+function dfsCount(root, count = 0) {
+  if (!root) return count;
+  count++;
+  count = dfsCount(root.left, count);
+  return dfsCount(root.right, count);
+}
 
 const testRunner = (tests, func) => {
   const name = func.name;
   for (const [treeArray, expected] of tests) {
     let input = new Tree(treeArray);
-    const result = func(input);
+    const result = func(input.root);
     console.log({
       name,
       input: input.toArray(),
@@ -27,6 +36,8 @@ const testRunner = (tests, func) => {
 
 const test = () => {
   const tests = [
+    [[10],1],
+    [[10,11,-4],3],
     [[1, 2, 3, 4, 5, 6], 6],
     [[1, 2, 3, 4], 4],
     [[1, 2, 3, 4, 5, 6, 7], 7],
