4ac8b2419eac3cfc7e02ef12fa50cd517163805a
Jesse Van Volkinburg <vanvoljg@gmail.com>
2020-06-24 16:48:44 -0700

    mathematical definiton
    The calculation in question is "Catalan's Number", which is defined as the product, from `k=2` to `n`, of `(k + n) / n`. There are other solutions as well, which could perform this recursively and cache the result of function calls. There would end up being `lg(n)` function calls on the stack, though, so I want to avoid that...

--- leetcode_june/24-unique_binary_search_trees.js
+++ leetcode_june/24-unique_binary_search_trees.js
@@ -1,7 +1,43 @@
+'use strict';
+
 /**
  * @param {number} n
  * @return {number}
  */
 function numTrees(n) {
-  
+  if (n < 2) return 1;
+  let ret = 1;
+  for (let i = 2; i <= n; i++) {
+    ret = (ret * (n + i)) / i;
+  }
+  return ret;
+}
+
+function testRunner(tests, func) {
+  const name = func.name;
+  for (const [input, expected] of tests) {
+    const result = func(input);
+    console.log({
+      name,
+      input,
+      expected,
+      result,
+      pass: result === expected,
+    });
+  }
+}
+
+const test = () => {
+  const tests = [
+    [1, 1],
+    [2, 2],
+    [3, 5],
+    [4, 14],
+    [5, 42],
+    [6, 132],
+  ];
+
+  testRunner(tests, numTrees);
 };
+
+export default test();
