e07f97f6bca623efe769006774adf197f235a0f8
Jesse Van Volkinburg <vanvoljg@gmail.com>
2020-06-26 13:02:05 -0700

    add tests and runner

--- leetcode_june/26-sum_root_to_leaf_nodes.js
+++ leetcode_june/26-sum_root_to_leaf_nodes.js
@@ -1,3 +1,7 @@
+'use strict';
+
+import {Tree} from './lib/tree.js';
+
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
@@ -11,5 +15,33 @@
  * @return {number}
  */
 function sumNumbers(root) {
-  
+  if (!root) return 0;
 };
+
+function testRunner(tests, func) {
+  const name = func.name;
+  for (const [treeArray, expected] of tests) {
+    let input = new Tree(treeArray);
+    const result = func(input.root);
+    console.log({
+      name,
+      input: input.toArray(),
+      result,
+      expected,
+      pass: result === expected,
+    });
+  }
+}
+
+const test = () => {
+  const tests = [
+    [[], 0],
+    [[1], 1],
+    // [[1,2,3], 25],
+    // [[4,9,0,5,1], 1026],
+  ];
+
+  testRunner(tests, sumNumbers);
+};
+
+export default test();
