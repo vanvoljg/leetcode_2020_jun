cb33b63d580e7c62de21822439dda86d1404a423
Jesse Van Volkinburg <vanvoljg@gmail.com>
2020-06-27 09:05:32 -0700

    add tests and runner

--- leetcode_june/27-perfect_squares.js
+++ leetcode_june/27-perfect_squares.js
@@ -1,7 +1,35 @@
+'use strict';
+
 /**
  * @param {number} n
  * @return {number}
  */
-function numSquares (n) {
+function numSquares(n) {
   
 };
+
+function testRunner(tests, func) {
+  const name = func.name;
+  for (let [input, expected] of tests) {
+    const result = func(input);
+    console.log({
+      name,
+      input,
+      result,
+      expected,
+      pass: result === expected,
+    });
+  }
+}
+
+function test() {
+  const tests = [
+    [12, 3],
+    [13, 2],
+    [143, 4],
+  ];
+
+  testRunner(tests, numSquares);
+}
+
+export default test();
