8aca1af68182ec62c44280e34d3a041adb6b4a9a
Jesse Van Volkinburg <vanvoljg@gmail.com>
2020-06-28 12:37:55 -0700

    add tests and runner

--- leetcode_june/28-reconstruct_itinerary.js
+++ leetcode_june/28-reconstruct_itinerary.js
@@ -7,3 +7,34 @@
 const findItinerary = (tickets) => {
   
 };
+
+const testRunner = (tests, func) => {
+  const name = func.name;
+  for (let [input, expected] of tests) {
+    const result = func(input);
+    console.log({
+      name,
+      input,
+      result,
+      expected,
+      pass: result === expected,
+    });
+  }
+}
+
+const test = () => {
+  const tests = [
+    [
+      [["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]],
+      ["JFK", "MUC", "LHR", "SFO", "SJC"],
+    ],
+    [
+      [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]],
+      ["JFK","ATL","JFK","SFO","ATL","SFO"],
+    ],
+  ];
+
+  testRunner(tests, findItinerary);
+}
+
+export default test();
