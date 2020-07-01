ac948379d7753bd542a76002d1d6de1c6149d21b
Jesse Van Volkinburg <vanvoljg@gmail.com>
2020-06-30 23:36:57 -0700

    algo complete
    This... this was not very clear. Tries are weird.

--- leetcode_june/30-word_search_ii.js
+++ leetcode_june/30-word_search_ii.js
@@ -1,6 +1,23 @@
 'use strict';
 
-import { Trie } from './lib/trie.js';
+const Trie = function() {
+  this.children = {};
+  this.word = null;
+}
+
+Trie.prototype.insert = function(word) {
+  let node = this;
+  let char = '';
+  for (char of word) {
+    if (!node.children[char]) node.children[char] = new Trie();
+    node = node.children[char];
+  }
+  node.word = word;
+}
+
+Trie.prototype.build = function(words) {
+  for (let word of words) this.insert(word);
+}
 
 /**
  * @param {character[][]} board
@@ -13,8 +30,7 @@ const findWords = (board, words) => {
   if (!m || !n) return [];
 
   let trie = new Trie();
-  for (let word of words) trie.insert(word);
-
+  trie.build(words);
   let ret = [];
 
   for (let i = 0; i < m; i++) {
@@ -22,11 +38,23 @@ const findWords = (board, words) => {
       dfs(board, i, j, trie, ret);
     }
   }
-  return ret;
+  return ret.reverse();
 };
 
 const dfs = (board, i, j, trie, ret) => {
-  console.log(trie.startsWith(board[i][j]));
+  const char = board[i][j];
+  if (char === '' || !trie.children[char]) return;
+  trie = trie.children[char];
+  if (trie.word) {
+    ret.push(trie.word); // add found word to return array
+    trie.word = null; // prevent this word from being selected again
+  }
+  board[i][j] = '';
+  if (i > 0) dfs(board, i - 1, j, trie, ret); // 1 row up
+  if (j > 0) dfs(board, i, j - 1, trie, ret); // 1 col left
+  if (i < board.length - 1) dfs(board, i + 1, j, trie, ret);
+  if (j < board[0].length - 1) dfs(board, i, j + 1, trie, ret);
+  board[i][j] = char; // backtrack
 };
 
 const testRunner = (tests, func) => {
@@ -38,9 +66,16 @@ const testRunner = (tests, func) => {
       input,
       result,
       expected,
-      pass: result === expected,
+      pass: arrtest(result, expected),
     });
   }
+};
+
+const arrtest = (result, expected) => {
+  for (let i = 0; i < expected.length; i++) {
+    if (result[i] !== expected[i]) return false;
+  }
+  return true;
 };
 
 const test = () => {
